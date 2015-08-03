a8ef61d46a21d438c9ccf746a436dd386a6ba902
Yuan Wang <ywang@Yuans-MBP.home>
2015-08-02 22:41:33 -0400

    =added first_run script

---
+++ first_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "SCRIPT START"
+echo "Make sure you have installed Python, pip, and git"
+pip install ipython
+pip install pyalgotrade
+pip install sklearn
+git
+
+mkdir ~/dev
+cd ~/dev
+git clone https://github.com/yuanagain/gimg_btn.git
+echo "SCRIPT COMPLETE"
