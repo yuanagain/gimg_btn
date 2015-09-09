9be1b91df7bac91f98b7617ed6fae530b31bc1ee
W <yuanw@princeton.edu>
2015-09-08 20:22:06 -0400

    gag stuff

---
+++ evil_finance.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import time
+print "Computing actuaries for developing world ORPHANS:"
+time.sleep(1)
+print "......"
+time.sleep(1)
+print "......"
+time.sleep(1)
+print "......"
+time.sleep(1)
+print "COMPLETE"
+print "Redisplacing IDPs"
+print "......"
+time.sleep(1)
+print "......"
+time.sleep(1)
+print "COMPLETE"
+print "EVIL => MONEY"
+time.sleep(.5)
+print "EVIL => MONEY"
+time.sleep(.5)
+print "EVIL => MONEY"
+time.sleep(.5)
+print "EVIL => MONEY"
+time.sleep(.5)
+print "EVIL => MONEY"
+time.sleep(.3)
+print "BLING BLING BLING"
+os.system("python tutorialPlotter.py")
+os.system("clear")
